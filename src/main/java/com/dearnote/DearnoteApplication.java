c07483a6db854c8f81386b8ba2065fff8b8dcb66
sshnote <springssh0504@naver.com>
2024-12-16 21:51:21 +0900

    first commit

---
+++ src/main/java/com/dearnote/DearnoteApplication.java
@@ -0,0 +1,13 @@
+package com.dearnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DearnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dearnote/DearnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
