c5e969f87b4c08594dcccc530278cbabf27c3301
최경환 <choegyeonghwan@choegyeonghwan-ui-MacBookAir.local>
2024-12-17 13:38:39 +0900

    domain 설계

---
+++ dearnote/src/main/java/umc/umc/domain/Letter.java
@@ -0,0 +1,23 @@
+package umc.umc.domain;
+
+import jakarta.persistence.*;
+import umc.domain.common.BaseEntity;
+import umc.domain.enums.LetterType;
+
+public class Letter extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'RECEIVED'")
+    private LetterType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "letterBox_id", nullable = false)
+    private LetterBox letterBox;
+}
---
+++ dearnote/src/main/java/umc/umc/service/MemberQueryService.java
@@ -0,0 +1,4 @@
+package umc.umc.service;
+
+public interface MemberQueryService {
+}
