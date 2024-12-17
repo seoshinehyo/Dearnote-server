7187b1a1d4fc153291f8cce4750c7fbf485e2214
sshnote <springssh0504@naver.com>
2024-12-17 21:29:20 +0900

    feat: 스웨거 세팅

--- src/test/java/com/dearnote/DearnoteApplicationTests.java
+++ src/test/java/com/dearnote/DearnoteApplicationTests.java
@@ -3,7 +3,7 @@ package com.dearnote;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DearnoteApplication.class)
 class DearnoteApplicationTests {
 
     @Test
