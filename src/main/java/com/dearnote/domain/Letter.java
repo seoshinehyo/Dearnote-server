f276fbf901ccf828292442c3211f0c4133cfdb5c
최경환 <choegyeonghwan@choegyeonghwan-ui-MacBookAir.local>
2024-12-18 17:33:43 +0900

    domain 수정

--- src/main/java/com/dearnote/domain/Letter.java
+++ src/main/java/com/dearnote/domain/Letter.java
@@ -28,6 +28,15 @@ public class Letter extends BaseEntity {
     @Column(columnDefinition = "VARCHAR(15)")
     private LetterStatus status;
 
+    @Column(nullable = true, columnDefinition = "VARCHAR(100)")
+    private String imageDescription;
+
+    // 글씨 폰트
+
+    // 편지지
+
+    // 실링 왁스
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "letterBox_id", nullable = false)
     private LetterBox letterBox;
