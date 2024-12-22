cfd6788c739b5b36fa2dcd29890b1b03acd4f1d7
jjun522 <tjwowns522@>
2024-12-22 23:22:29 +0900

    cors 설정

--- src/main/java/com/dearnote/config/WebConfig.java
+++ src/main/java/com/dearnote/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
                 .allowedOrigins("http://localhost:5173");
     }
 }
